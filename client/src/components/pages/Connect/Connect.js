243b86940fbc398418448ccb27cf8d64ff326cd7
Matthew Murnighan <mattmurn@gmail.com>
2018-04-30 10:50:46 -0500

    updated with db

--- client/src/components/pages/Connect/Connect.js
+++ client/src/components/pages/Connect/Connect.js
@@ -4,7 +4,7 @@ import Skills from '../../Skills'
 import IoSocialGithub from 'react-icons/lib/io/social-github';
 import IoSocialLinkedin from 'react-icons/lib/io/social-linkedin';
 //uncomment when you have express.js serving you.
-// const nodeMailer = require('nodemailer');
+ import axios from 'axios'
 // look at using the higher oder components in a refactor...
 
 class Connect  extends Component{
@@ -13,7 +13,10 @@ class Connect  extends Component{
         this.state = {
             name: "",
             email: "",
-            message: ""
+            message: "",
+            placeholderName: "Name",
+            placeholderEmail: "Email",
+            placeholderMessage: "Message"
 
         }
         // this.getConnecData = this.getConnectData.bind(this);
@@ -30,56 +33,62 @@ class Connect  extends Component{
         // console.log(this.state)
 
     }
-    sendConnectEmail = (event, getConnectData) => {
+    sendConnectEmail = (event) => {
         event.preventDefault();
-        // console.log("send connect email");
-        let key = process.env.GMAIL;
+        if ( 1> this.state.name.length > 30 ){
+            this.setState({ 
 
-        // const transporter = nodeMailer.createTransport({
-        //     service: 'gmail',
-        //     auth: {
-        //         user: 'nodemailermurn@gmail.com',
-        //         pass: key
-        //     }
-        // });
-        // getConnectData(event)
-        const mailOptions = {
-            from: this.state.email,
-            to: 'mattmurn@gmail.com',
-            subject: `Portfolio Message from ${this.state.name}`,
-            text: this.state.message
-        };
+                name:"",
+                placeholderName: "Please enter your name"
+            });
+            return;
+            
+        }
+        else if (1 > this.state.message > 5000 ) {
+            this.setState({ 
 
-        // transporter.sendMail(mailOptions, function(error, info){
-        //     if(error){ throw error }
-        //     else{
-        //         console.log('Email sent: ' + info.response);
-        //     }
-        // })
-        console.log(mailOptions);
-        this.setState({
-                        name: "",
-                        email: "",
-                        message: ""
-                    })
-    }
+                message: "",
+                placeholderMessage: "Hey. I'd really like to know more about you."
+            });
+            return;
+        }
+        else{
+            axios.post('/api/connects', {
+
+                name: this.state.name,
+                email: this.state.email, 
+                message: this.state.message
+
+            }).then(
+
+                this.setState({
+                    name: "",
+                    email: "",
+                    message: ""
+                })            
+            )
+        }
+
+
+    }   ;
     render() {
-    
-        return(
+        const { placeholderName, placeholderEmail, placeholderMessage, name, email, message } = this.state;
+        
+            return(
 
             <div className={this.props.connectClass}>
                 <a className="connectATag" href="https://www.linkedin.com/in/matthew-murnighan/" target="_blank"><Skills techLogo={<IoSocialLinkedin size={32}/>}/></a>
                 <a className="connectATag" href="https://github.com/MattMurn" target="_blank"><Skills techLogo={<IoSocialGithub size={32}/>}/></a>
                 <div className="connect_name">
-                    <input type="text" id="inq_name" name="name" placeholder="Name" value={this.state.name} onChange={this.getConnectData}/>
+                    <input type="text" id="inq_name" name="name" placeholder={placeholderName} value={name} onChange={this.getConnectData}/>
                 </div>
                 
                 <div className="connect_email">
-                    <input type="email" id="inq_email" name="email"  placeholder="Email" value={this.state.email} onChange={this.getConnectData}/>
+                    <input type="email" id="inq_email" name="email"  placeholder={placeholderEmail} value={email} onChange={this.getConnectData}/>
                 </div>
                 
                 <div className="connect_text_box">
-                    <textarea id="inq_subject" name="message" placeholder="Message" value={this.state.message} onChange={this.getConnectData}></textarea>
+                    <textarea id="inq_subject" name="message" placeholder={placeholderMessage} value={message} onChange={this.getConnectData}></textarea>
                     <button type="submit" className="submit" onClick={this.sendConnectEmail}>Send</button>
                 </div>
 
