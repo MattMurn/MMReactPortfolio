259e9e3aa8df1fabaebd6574954ccafcf6f8be8b
Matthew Murnighan <mattmurn@gmail.com>
2019-01-16 11:02:32 -0600

    updated queries

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = props => {
     let date = new Date();
     return(
         <div className="footer">
-            <div className="theme_color_light footer_contact"> TELEPHONE 312 835 0105 | EMAIL MATTMURN@GMAIL.COM © {date.getFullYear().toString()}</div>
+            <div className="theme_color_light"> TELEPHONE 312 835 0105 | EMAIL MATTMURN@GMAIL.COM © {date.getFullYear().toString()}</div>
         </div>
     );
 }
