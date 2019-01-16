0427df38080e10ab5dde4ff252a61b91cf5caa13
Matthew Murnighan <mattmurn@gmail.com>
2019-01-15 21:28:01 -0600

    confirmed changes for ipFn

--- microservices/IpLookUp/IpFn.js
+++ microservices/IpLookUp/IpFn.js
@@ -5,7 +5,6 @@ const db = require('../../models')
 module.exports = function(app)  {
     app.post('/ipInfo', (req, res) => {
         let ip = req.body.ip;
-<<<<<<< HEAD
         axios.get(`http://api.ipstack.com/${ip}?access_key=e3af7c52287a4cc971a2caa31e54fb6b`)
         .then(data => {
             let i = data.data;
@@ -20,27 +19,6 @@ module.exports = function(app)  {
                 time: moment().format('MMMM Do YYYY, h:mm:ss a')
             }
             db.ipInfo.create(userIp).then(data => console.log(data));
-=======
-    //    console.log(`response: ${res.body}, request: ${req.body.ip}`)
-            axios.get(`http://api.ipstack.com/${ip}?access_key=e3af7c52287a4cc971a2caa31e54fb6b`)
-            .then(data => {
-                let i = data.data;
-                let userIp = {
-                    ip: i.ip,
-                    country_name: i.country_name,
-                    region_name: i.region_name,
-                    city: i.city,
-                    zip: i.zip,
-                    lat: i.latitude,
-                    long: i.longitude,
-                    time: JSON.stringify(new Date())
-                }
-                console.log(`IP USER INFORMATION IS: ${userIp.city}`);
-                //make db post here.
-                db.ipInfo.create(userIp).then(data => console.log(data));
-                res.send(userIp.city);
-            })
->>>>>>> refile
         })
     })
 }
