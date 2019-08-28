2c0d41c63ae7e3fb82f0a93118a057130ef26d18
Matthew Murnighan <mattmurn@gmail.com>
2019-08-27 23:04:45 -0500

    stripping out garbage

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,39 +2,32 @@ import React from 'react'
 import './About.css'
 
 const About = props => (
-        
-        <div className={props.className}> 
+
+        <div className="blurb blurb_about about__flex">
             <div className="about_title theme_family"> Web Developer</div>
-                <p className="about_body theme_family">
-                        I'm a developer who enjoys the puzzle of a well-run application and am looking for a 
-                        development opportunities that will allow me to collaborate and help solve some of your company's problems. 
+                <div className="about_body theme_family">
+                        I'm a developer who enjoys the puzzle of a well-run application.
                         Previously I developed, tested and deployed automated trading strategies in several derivative markets.
-                        Currently, I am winding down a consulting contract on a software trade secrets litigation matter building a native UI application that 
-                        allows our team access to trial exhibits in various doucment formats, along with topic specific summary charts.
-                        I am also a Teaching Assistant at Northwestern University Coding Bootcamp. Being a part of the instructional staff 
-                        allows me to help students stay motivated, engaged & focused, while maintaining best practices and continuing to learn 
-                        emerging technologies. This site is a fullstack application in React, Node.js, MySQL, Express stack. 
-                </p>
+                        Currently, I am web developer at <a className="tst-link" rel="noopener noreferrer"  href="https://www.topsteptrader.com/" target="_blank">TopstepTrader</a> a fintech company that provides frunding to retail derivatives traders.
+                        I am also a Teaching Assistant at <a className="nu-link" rel="noopener noreferrer"  href="https://bootcamp.northwestern.edu/coding/">Northwestern University Coding Bootcamp</a>. Being a part of the instructional staff
+                        allows me to help students stay motivated, engaged & focused, while maintaining best practices and continuing to learn
+                        emerging technologies. This site is a fullstack application in React, Node.js, MySQL, Express stack.
+                </div>
                 <div className="about_skills theme_family">
-                <div className="about_tech"> Technologies & Skills </div>
-                        <div className="icon_container">
+                        <div className="icon__container">
                                 <i className=" icon devicon-bootstrap-plain-wordmark colored"></i>
                                 <i className=" icon devicon-css3-plain-wordmark colored"></i>
                                 <i className=" icon devicon-express-original-wordmark"></i>
                                 <i className=" devicon-git-plain colored icon"></i>
-                                {/* <i className=" icon devicon-git-plain-wordmark colored"></i> */}
-                                <i className=" devicon-html5-plain colored icon"></i>
-                                <i className=" icon devicon-heroku-original-wordmark colored"></i>
                                 <i className=" icon devicon-jquery-plain-wordmark colored"></i>
-                                <i className=" icon devicon-mongodb-plain-wordmark colored"></i>
                                 <i className=" icon devicon-mysql-plain-wordmark colored"></i>
                                 <i className=" icon devicon-nodejs-plain colored"></i>
                                 <i className=" icon devicon-python-plain colored"></i>
                                 <i className=" icon devicon-react-original-wordmark colored"></i>
                                 <i className=" icon devicon-sequelize-plain-wordmark colored"></i>
                                 <i className=" icon devicon-javascript-plain colored"></i>
-                                <i className=" icon devicon-html5-plain-wordmark colored"></i>  
-                        </div>                   
+                                <i className=" icon devicon-html5-plain-wordmark colored"></i>
+                        </div>
                 </div>
         </div>
 )
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './Landing.css'
 import Navbar from '../Navbar'
 import Blurb from '../Blurb'
 import About from '../About'
-import { Project } from "../Project/Project" 
-// import Chat from './Chat'
+import { Name, Initials } from '../Name/Name'
 import Footer from '../Footer';
-import { Name , Initials} from '../Name/Name'
-import {Connect, InfoLink} from '../Connect/Connect'
-import projects from '../../projects.json'
-import IoSocialGithub from 'react-icons/lib/io/social-github';
-// import { Document, Page } from 'react-pdf';
-import axios from 'axios';
 
 class Landing extends Component {
-    constructor(props){
-        super(props);   
+    constructor(props) {
+        super(props);
         this.state = {
             projects: [],
             class: false,
@@ -26,80 +18,31 @@ class Landing extends Component {
         }
         this.myRef = React.createRef();
     }
-    componentWillMount = () => {
-
-        this.setState({projects: projects});
-        this.getIp();
-        this.setState({resume: "./assets/img/Murnighan_Resume.pdf"})
-    }   
-    clickEvent = event => {        
+    clickEvent = event => {
 
         window.scrollTo({
-            top: parseInt(event.target.id), 
+            top: parseInt(event.target.id),
             behavior: "smooth"
         })
     }
-    getIp = () => {
-
-        axios.get('http://getcitydetails.geobytes.com/GetCityDetails?fqcn=',{ headers: {
-            'Access-Control-Allow-Origin': '*'
-          }})
-        .then(response => {
-
-            let ipA = response.data.geobytesipaddress;
-            localStorage.setItem('ipAddress', ipA);
-            axios.post('/ipInfo', {ip: ipA});
-        })
-    }
     render() {
-        
-       const { projects } = this.state;
-        return(
+
+        return (
             <Router>
                 <div>
-                    <Navbar clickHandler={this.clickEvent} className='nav_dropdown_inner theme_background_light'/> 
-                    <Initials/>
-                    <Blurb className=" blurb blurb_title" onMouseEnter={this.mouseEnter}>
-                    <Name   
-                        wrapper="first_name_wrapper"
-                        first="Matthew"
-                        last="Murnighan"
-                    />
+                    <Navbar clickHandler={this.clickEvent} className='nav_dropdown_inner theme_background_light' />
+                    <Initials />
+                    <Blurb className=" blurb blurb_title">
+                        <Name
+                            wrapper="first_name_wrapper"
+                            first="Matthew"
+                            last="Murnighan"
+                        />
                     </Blurb>
                     <Blurb className="blurb blurb_about" >
-                        <About onMouseEnter={this.mouseEnter}/>
+                        <About/>
                     </Blurb>
-                    <Route path="/projects">
-                        <div>
-                            {projects.map((proj, i)=> ( 
-                                <Blurb className="blurb dark project">   
-                                    <Project                                        
-                                        id={projects[i].id}
-                                        key={i}
-                                        src={projects[i].src}
-                                        description={projects[i].description}
-                                        deployment={projects[i].deployment}
-                                        title={projects[i].title}
-                                        github={projects[i].gitHub}
-                                        logo={<IoSocialGithub size={25}/>}
-                                        img_class={(projects[i].css_class) ?
-                                        `project_outer ${projects[i].css_class}` : 
-                                        'project_outer'}
-                                        />
-                                </Blurb>
-                            ))}
-                        </div>
-                    </Route>
-                    <Blurb className="blurb blurb_connect">
-                        <InfoLink/>
-                    </Blurb>
-                    <Route path="/connect"
-                        render={() => 
-                            <Blurb className="blurb blurb_inq">
-                                <Connect/>
-                            </Blurb>     
-                        }/>
-                        <Footer/>
+                    <Footer />
                 </div>
             </Router>
         )
